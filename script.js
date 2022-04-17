9b785301bc764be085d0ebda75750de04634e2d3
Tyler Lampel <lampel13@gmail.com>
2022-04-16 22:53:41 -0400

    finish imgs, add links, add sounds

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Welcome!";
 document.querySelector("body").appendChild(h2);
+
+var audio = $("#brother")[0];
+$("nav a").mouseenter(function() {
+  audio.play();
+});
