203f20731cf9f23ba0a60f3387bdadc2d0cb532e
Tyler Lampel <lampel13@gmail.com>
2022-04-17 11:01:39 -0400

    redo sound buttons, prep css

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const h2 = document.createElement("h2");
 h2.textContent = "Welcome!";
 document.querySelector("body").appendChild(h2);
 
-var audio = $("#brother")[0];
-$("nav a").mouseenter(function() {
-  audio.play();
-});
+let playSound = () => new Audio("./sounds/BROTHER!-Hulk_Hogan.mp3").play()
+let playSound1 = () => new Audio("./sounds/WaterDropletSound.mp3").play()
+let playSound2 = () => new Audio("./sounds/explosion.mp3").play()
+let playSound3 = () => new Audio("./sounds/WoodSandingSoundEffect.mp3").play()
+let playSound4 = () => new Audio("./sounds/JUJUTSU_KAISEN.mp3").play()
+let playSound5 = () => new Audio("./sounds/MY_HERO_ACADEMIA.mp3").play()
+let playSound6 = () => new Audio("./sounds/Demon_Slayer_Title_Card_Sound_Effect.mp3").play()
+let playSound7 = () => new Audio("./sounds/Luffy_laugh_one_piece.mp3").play()
+let playSound8 = () => new Audio("./sounds/Thank_you_soundeffect.mp3").play()
